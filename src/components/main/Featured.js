355ea782018ed88ed3f69e6ffdf6a20e28bbacbf
Stephan <sgriesel@gmail.com>
2021-09-12 08:00:27 +0200

    styles & add sample data

--- src/components/main/Featured.js
+++ src/components/main/Featured.js
@@ -4,13 +4,13 @@ import styled from 'styled-components';
 const Featured = () => {
   return (
     <FeatDiv>
-      <div className='half'>
+      <div className='item'>
         <img
           src='https://images.pexels.com/photos/821651/pexels-photo-821651.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
           alt='product'
         />
       </div>
-      <div className='half'>
+      <div className='item'>
         <h1>Featured Product</h1>
         <p>Camera</p>
       </div>
@@ -19,16 +19,20 @@ const Featured = () => {
 };
 
 const FeatDiv = styled.div`
-  display: flex;
-  .half {
-    max-width: 50%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    height: 100%;
-    img {
-      width: 70em;
-    }
+  display: grid;
+  grid-template-columns: 1fr;
+  align-content: flex-start;
+  gap: 1em;
+  padding: 1em;
+  .item {
+    padding: 5.5em 1em;
+  }
+  img {
+    width: 100%;
+  }
+
+  @media (min-width: 992px) {
+    grid-template-columns: 1fr 1fr;
   }
 `;
 
---
+++ src/shop/index.js
@@ -0,0 +1,29 @@
+const SHOP_DATA = [
+  {
+    id: 1,
+    title: 'Camera Bag',
+    description:
+      'Bag for your stylish camera with extra padding so you can protect it',
+    imageUrl:
+      'https://images.pexels.com/photos/2081199/pexels-photo-2081199.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    price: 10,
+  },
+  {
+    id: 2,
+    title: 'Hip shirt',
+    description: 'Shirt to match you fancy outfit with your cool camera',
+    imageUrl:
+      'https://images.pexels.com/photos/428340/pexels-photo-428340.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    price: 5,
+  },
+  {
+    id: 3,
+    title: 'Trendy Shorts',
+    description: 'Comfy shorts for every day sunny weather',
+    imageUrl:
+      'https://images.pexels.com/photos/3622624/pexels-photo-3622624.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=12600',
+    price: 5,
+  },
+];
+
+export default SHOP_DATA;
