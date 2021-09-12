1471714b6a007ce1ab6f4143d0ec6541c65e2586
Stephan <sgriesel@gmail.com>
2021-09-12 13:59:32 +0200

    refactor & new layout component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,12 @@
 import * as React from 'react';
 import '../css/styles.css';
-import Header from '../components/header/Header';
-import Hero from '../components/hero/Hero';
-import Main from '../components/main/Main';
-import Footer from '../components/footer/Footer';
-import FeaturedCollection from '../components/featured-collection/featured-collection';
+import HomePage from '../components/homepage';
 import ProductsContextProvider from '../context/products-context';
 
 const IndexPage = () => {
   return (
     <ProductsContextProvider>
-      <main>
-        <Header />
-        <Hero />
-        <Main />
-        <FeaturedCollection />
-        <Footer />
-      </main>
+      <HomePage />
     </ProductsContextProvider>
   );
 };
