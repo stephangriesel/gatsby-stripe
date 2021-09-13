40d0e7a3fbfb636eed846a52bd0894fdee2648ba
Stephan <sgriesel@gmail.com>
2021-09-13 07:23:51 +0200

    add shop component

---
+++ src/pages/shop.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Shop from '../components/shop/Shop'
+
+const ShopPage = () => {
+    return (
+        <div>
+            <Shop />
+        </div>
+    )
+}
+
+export default ShopPage
